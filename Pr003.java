6b7232d97867ca6827485be2094d7bdf50cc7593
zazovskaya <zazovskaya2004@gmail.com>
2025-09-09 12:14:42 +0700

    Добавили логический тип данных

--- Pr003.java
+++ Pr003.java
@@ -77,5 +77,20 @@ class Pr003{
 			System.out.print(ch++ + ", ");
 		}
 		System.out.println();
+
+		//Использование логического типа данных
+		varb = false; 
+		System.out.println("Значение varb: " + varb);
+
+		varb = true;
+		System.out.println("Значение varb: " + varb);
+
+		if(varb)
+			System.out.println("Выполняется, если varb == true");
+		varb = false;
+		if(varb)
+			System.out.println("Выполняется, если varb == true");
+		
+		System.out.println("Результат сравнения 10>9: " + (10>9));
 	}
 }
