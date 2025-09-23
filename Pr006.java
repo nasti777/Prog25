71ee74c31b98c2c68eefae8aafa74eb03e4a5032
zazovskaya <zazovskaya2004@gmail.com>
2025-09-23 18:35:05 +0700

    Первый пример цикла for

--- Pr006.java
+++ Pr006.java
@@ -108,5 +108,19 @@ class Pr006 {
 				System.out.println("Значение по умолчанию для внешнего switch()");
 		}
 
+		//оператор цикла for
+		System.out.println();
+		double num, sroot, rerr;
+
+		for(num = 1.0; num < 100.0; num++) {
+			sroot = Math.sqrt(num);
+			System.out.println("Квадратный корень из " + num + " равен " + sroot);
+
+			//вычисление ошибки округления
+			rerr = num - (sroot * sroot);
+			System.out.println("Ошибка округления: " + rerr);
+			System.out.println();
+		}
+
 	}
 }
