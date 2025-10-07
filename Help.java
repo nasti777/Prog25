2b8a58b16b746f82366d8416b29410029c45064b
nasti777 <zazovskaya2004@gmail.com>
2025-10-07 12:27:30 +0700

    Финальная структурная версия программы Help

--- Help.java
+++ Help.java
@@ -3,55 +3,71 @@ class Help {
 	  throws java.io.IOException {
 		char choise, temp;
 
+		  
+		for(;;) {
 		do {
 
-			System.out.println("Справочная система");
-			System.out.println(" 1. if");
-			System.out.println(" 2. switch");
-			System.out.println(" 3. for");
-			System.out.println(" 4. while");
-			System.out.println(" 5. do-while");
+				System.out.println("Справочная система");
+				System.out.println(" 1. if");
+				System.out.println(" 2. switch");
+				System.out.println(" 3. for");
+				System.out.println(" 4. while");
+				System.out.println(" 5. do-while");
+				System.out.println(" 6. break");
+				System.out.println(" 7. continue");
 
-			System.out.print("Выберите нужное число: ");
-			choise = (char) System.in.read();
+				System.out.print("Выберите номер нужного пункта (для выхода нажмите q): ");
+				choise = (char) System.in.read();
+			
+				do {
+					temp = (char) System.in.read();
+				} while (temp != '\n');
+			} while (choise < '1' | choise > '7' & choise != 'q');
 
-			do {
-				temp = (char) System.in.read();
-			} while (temp != '\n');
-		} while (choise < '1' | choise > '5');
+			if(choise == 'q')
+				break;
 
-		System.out.println("\n");
+			System.out.println("\n");
 
-		switch(choise) {
-			case '1':
-				System.out.println("Оператор if:\n");
-				System.out.println("if(логическое выражение) оператор;");
-				System.out.println("else оператор;");
-				break;
-			case '2':
-				System.out.println("оператор switch:\n");
-				System.out.println("switch(выражение) {");
-				System.out.println(" case константа:");
-				System.out.println(" последовательность операторов");
-				System.out.println(" break;");
-				System.out.println(" // ...");
-				System.out.println("}");
-				break;
-			case '3':
-				System.out.println("Оператор for:\n");
-				System.out.println("for(инициализация; условие; итерация)");
-				System.out.println(" оператор;\n");
-				break;
-			case '4':
-				System.out.println("Оператор while:\n");
-				System.out.println("while(условие) оператор;");
-				break;
-			case '5':
-				System.out.println("Оператор do-while:\n");
-				System.out.println("do {");
-				System.out.println(" оператор;");
-				System.out.println("} while(условие);");
-				break;
+			switch(choise) {
+				case '1':
+					System.out.println("Оператор if:\n");
+					System.out.println("if(логическое выражение) оператор;");
+					System.out.println("else оператор;");
+					break;
+				case '2':
+					System.out.println("оператор switch:\n");
+					System.out.println("switch(выражение) {");
+					System.out.println(" case константа:");
+					System.out.println(" последовательность операторов");
+					System.out.println(" break;");
+					System.out.println(" // ...");
+					System.out.println("}");
+					break;
+				case '3':
+					System.out.println("Оператор for:\n");
+					System.out.println("for(инициализация; условие; итерация)");
+					System.out.println(" оператор;\n");
+					break;
+				case '4':
+					System.out.println("Оператор while:\n");
+					System.out.println("while(условие) оператор;");
+					break;
+				case '5':
+					System.out.println("Оператор do-while:\n");
+					System.out.println("do {");
+					System.out.println(" оператор;");
+					System.out.println("} while(условие);");
+					break;
+				case'6':
+					System.out.println("Оператор break:\n");
+					System.out.println("break; или break метка;");
+				case '7':
+					System.out.println("Оператор continue:\n");
+					System.out.println("continue; или continue метка;");
+					break;
+			}
+			System.out.println();
 		}
 	}
 }
