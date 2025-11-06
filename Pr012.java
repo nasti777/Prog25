41f95e4f58d8ee5b942513a952eeac1bb95b91d6
zazovskaya <zazovskaya2004@gmail.com>
2025-11-06 12:36:22 +0700

    Перегрузка методов. Часть 1

--- Pr012.java
+++ Pr012.java
@@ -124,6 +124,41 @@ class ErrorInfo {
 			return new Err("Неверное значение ошибки", 0);
 	}
 }
+class Overload {
+	void ovlDemo() {
+		System.out.println("метод запуска без параметров");
+	}
+	void ovlDemo(int a) {
+		System.out.println("Один параметр: " + a);
+	}
+
+	int ovlDemo(int a, int b) {
+		System.out.println("Два параметра: " + a + " " + b);
+		return a+b;
+	}
+
+	double ovlDemo(double a, double b) {
+		System.out.println("Два вещественных параметра: " + a + " " + b);
+		return a+b;
+	}
+}
+class Square {
+	//вычисление площади квадрата 
+	int square(int a) {
+		System.out.println("Площадь квадрата со стороной " + a + " равна " + a*a);
+		return a*a;
+	}
+	//вычисление площади круга
+	double square(double r) {
+		System.out.println("Площадь круга с радиусом" + r + " равна " + 3.14*r*r);
+		return 3.14*r*r;
+	}
+	//вычисление плошади прямоугольника
+	int square(int a, int b) {
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + a*b);
+		return a*b;
+	}
+}
 class Pr012 {
 	public static void main(String arg[]) {
 		MyClass ob = new MyClass();
@@ -191,5 +226,34 @@ class Pr012 {
 
 		e = err1.getErrorInfo(7);
 		System.out.println(e.msg + " уровень: " + e.severity);
+
+		//Перегрузка методов ovlDemo()
+		System.out.println();
+		Overload ob4 =  new Overload();
+		int resI;
+		double resD;
+
+		ob4.ovlDemo();
+		System.out.println();
+
+		ob4.ovlDemo(3);
+		System.out.println();
+
+		ob4.ovlDemo(7,8);
+		System.out.println("Возвращаемое значение: " + ob4.ovlDemo(7,8));
+
+		ob4.ovlDemo(2.7,3.4);
+		System.out.println("Возвращаемое значение: " + ob4.ovlDemo(2.7,3.4));
+
+		//использование перегружаемого метода square() для вычисления площади различных фигур
+		System.out.println();
+		Square ob5 = new Square();
+		//Вычисление площади квадрата со стороны 4
+		int S_sq = ob5.square(4);
+		//Вычисление площади прямоугольника со сторонами 4 и 5
+		int S_rest = ob5.square(4,5);
+		//Вычисление площади круга с радиусом 3.0
+	        double S_ocr = ob5.square(3.0);	
+
 	}
 }
